66c7706b89a16a0344ef563fc1063c13f34b8ec2
Brayan Baquero <brayan.baquero.cz@gmail.com>
2021-02-01 22:11:34 -0500

    Agregar archivos de proyecto.

---
+++ Models/MockEmployeeRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementMVC.Models
+{
+    public class MockEmployeeRepository : IEmployeeRepository
+    {
+        private List<Employee> _employeeList;
+
+        public MockEmployeeRepository()
+        {
+            _employeeList = new List<Employee>()
+            {
+                new Employee(){Id=1,Name="Jessica",Deparment="HR", Email="jessica@gmail.com" },
+                new Employee(){Id=1,Name="Brayan",Deparment="Boss", Email="Brayan@gmail.com" },
+                new Employee(){Id=1,Name="Cristina",Deparment="HB", Email="Cristina@gmail.com" },
+            };
+        }
+        public Employee GetEmployee(int Id)
+        {
+            return _employeeList.FirstOrDefault(e => e.Id == Id);
+        }
+    }
+}
---
+++ controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using EmployeeManagementMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementMVC.controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+
+        public HomeController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+
+        }
+        public string index() {
+            //return Json(new {id=1,name="Prub" }); ;
+            return _employeeRepository.GetEmployee(1).Name;
+        }
+        public ViewResult Details()
+        {
+            //return Json(new {id=1,name="Prub" }); ;
+            Employee model = _employeeRepository.GetEmployee(1);
+            return View(model);
+        }
+    }
+}
