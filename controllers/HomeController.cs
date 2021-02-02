bad2cb354f67f36d8010e4e88c6f85324e055de9
Brayan Baquero <brayan.baquero.cz@gmail.com>
2021-02-02 11:01:18 -0500

    Strongly Typed View   https://www.youtube.com/watch?v=5auO0iXrOs4&list=PL6n9fhu94yhVkdrusLaQsfERmL_Jh4XmU&index=25

--- controllers/HomeController.cs
+++ controllers/HomeController.cs
@@ -24,7 +24,6 @@ namespace EmployeeManagementMVC.controllers
         {
             //return Json(new {id=1,name="Prub" }); ;
             Employee model = _employeeRepository.GetEmployee(1);
-            ViewBag.Employee = model;
             ViewBag.PageTitle = "Employee Details";
             return View(model);
         }
