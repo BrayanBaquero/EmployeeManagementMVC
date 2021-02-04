a3ead022f64059f09d827078330647c9a8ddc4eb
Brayan Baquero <brayan.baquero.cz@gmail.com>
2021-02-04 15:49:22 -0500

    Model Binding  https://www.youtube.com/watch?v=-GkZERrqEQo&list=PL6n9fhu94yhVkdrusLaQsfERmL_Jh4XmU&index=41&ab_channel=kudvenkat

--- controllers/HomeController.cs
+++ controllers/HomeController.cs
@@ -37,9 +37,16 @@ namespace EmployeeManagementMVC.controllers
             return View(homeDetailsViewModel);
         }
 
+        [HttpGet]
         public ViewResult Create()
         {
             return View();
+        }
+        [HttpPost]
+        public RedirectToActionResult Create(Employee employee)
+        {
+            Employee newEmployee=_employeeRepository.Add(employee);
+            return RedirectToAction("details",new { id=employee.Id});
         }
     }
 }
