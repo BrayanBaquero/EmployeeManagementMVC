e7a79c4390472b0435999ec364c3680101810e02
Brayan Baquero <brayan.baquero.cz@gmail.com>
2021-02-05 15:03:21 -0500

    Using sql server  https://www.youtube.com/watch?v=xMktEpPmadI&ab_channel=kudvenkat

--- Startup.cs
+++ Startup.cs
@@ -1,3 +1,4 @@
+using Microsoft.EntityFrameworkCore;
 using EmployeeManagementMVC.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -8,22 +9,33 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 
 namespace EmployeeManagementMVC
 {
     public class Startup
     {
+        private IConfiguration _config;
+
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            ////Inyectar dependencia que realizar gestion de base de datos con entity, y pasar la cadena de coneccion a base de datos
+            ///que se encuentra en appsettings.json
+            services.AddDbContextPool<AppDbContext>(options => options.UseSqlServer(_config.GetConnectionString("EmployeeDBConnection")));
+
             services.AddMvc(options => options.EnableEndpointRouting = false);
             //services.AddMvcCore(options => options.EnableEndpointRouting = false);
 
             //Inyeccion de dependencia, necesaria para que cuando se instancie la interface se incluya tambien su implementaicon
-            services.AddSingleton<IEmployeeRepository, MockEmployeeRepository>();
-            
-            
+            //services.AddSingleton<IEmployeeRepository, MockEmployeeRepository>();
+            services.AddTransient<IEmployeeRepository, MockEmployeeRepository>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
