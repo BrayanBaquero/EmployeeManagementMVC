49477307e7e9503e94bfa21e9bc6133ebb3c2dfe
Brayan Baquero <brayan.baquero.cz@gmail.com>
2021-02-10 16:21:05 -0500

    Handling 404 not found https://www.youtube.com/watch?v=QiF3eJ4Zb0o&ab_channel=kudvenkat

--- controllers/HomeController.cs
+++ controllers/HomeController.cs
@@ -33,9 +33,15 @@ namespace EmployeeManagementMVC.controllers
 
         public ViewResult Details(int? id)
         {
+            Employee employee = _employeeRepository.GetEmployee(id.Value);
+            if (employee == null)
+            {
+                Response.StatusCode = 404;
+                return View("EmployeeNotFound", id.Value); 
+            }
             HomeDetailsViewModel homeDetailsViewModel = new HomeDetailsViewModel()
             {
-                Employee=_employeeRepository.GetEmployee(id??1),
+                Employee=employee,
                 PageTitle="Employee Details"
             };
             
