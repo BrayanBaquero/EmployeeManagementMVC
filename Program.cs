a3379090cd6db5081669c817758cd99bd4c5f286
Brayan Baquero <brayan.baquero.cz@gmail.com>
2021-02-12 17:30:59 -0500

    Logging to file in asp net core using nlog https://www.youtube.com/watch?v=o5u4fE0t79k&ab_channel=kudvenkat

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using NLog.Extensions.Logging;
 
 namespace EmployeeManagementMVC
 {
@@ -21,6 +22,17 @@ namespace EmployeeManagementMVC
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                });
+                })
+            .ConfigureLogging((hostingContext, logging) =>
+            {
+                logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                logging.AddConsole();
+                logging.AddDebug();
+                logging.AddEventSourceLogger();
+                // Enable NLog as one of the Logging Provider
+                logging.AddNLog();
+            })
+            ;
+
     }
 }
