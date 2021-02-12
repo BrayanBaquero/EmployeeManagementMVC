e7cc2def3d851ff0dfc6f7553047e580124da5d6
Brayan Baquero <brayan.baquero.cz@gmail.com>
2021-02-12 17:00:43 -0500

    Logging exceptions  https://www.youtube.com/watch?v=LhpO2sgxqfw&ab_channel=kudvenkat

--- controllers/ErrorController.cs
+++ controllers/ErrorController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +11,13 @@ namespace EmployeeManagementMVC.controllers
 {
     public class ErrorController : Controller
     {
+        private readonly ILogger<ErrorController> logger;
+
+        public ErrorController(ILogger<ErrorController> logger)
+        {
+            this.logger = logger;
+        }
+
         [Route("Error/{statusCode}")]
         public IActionResult HttpStatusCodeHandler(int statusCode)
         {
@@ -18,8 +26,10 @@ namespace EmployeeManagementMVC.controllers
             {
                 case 404:
                     ViewBag.ErrorMessage = "Sorry, the resource you requested could not be found";
-                    ViewBag.Path = statusCodeResult.OriginalPath;
-                    ViewBag.QS = statusCodeResult.OriginalQueryString;
+                    logger.LogWarning($"404 error occured. Path={statusCodeResult.OriginalPath}" +
+                                        $" and QueryString={statusCodeResult.OriginalQueryString}");
+                    //ViewBag.Path = statusCodeResult.OriginalPath;
+                    //ViewBag.QS = statusCodeResult.OriginalQueryString;
                     break;
             }
             return View("NotFound");
@@ -29,9 +39,11 @@ namespace EmployeeManagementMVC.controllers
         public IActionResult Error()
         {
             var exceptionDetails = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
-            ViewBag.ExceptionPath = exceptionDetails.Path;
-            ViewBag.ExceptionMessage = exceptionDetails.Error.Message;
-            ViewBag.Stacktrace = exceptionDetails.Error.StackTrace;
+
+            logger.LogError($"The path{exceptionDetails.Path} threw an exception {exceptionDetails.Error}");
+            //ViewBag.ExceptionPath = exceptionDetails.Path;
+            //ViewBag.ExceptionMessage = exceptionDetails.Error.Message;
+            //ViewBag.Stacktrace = exceptionDetails.Error.StackTrace;
 
             return View("Error");
         }
