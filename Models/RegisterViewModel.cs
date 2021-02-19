e6baaaa149f02dc57d0551a57ef2301c25044332
Brayan Baquero <brayan.baquero.cz@gmail.com>
2021-02-19 16:49:25 -0500

    Register new user using asp net core identity https://www.youtube.com/watch?v=sPbDrqpme_w&ab_channel=kudvenkat

---
+++ Models/RegisterViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementMVC.Models
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public String Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public String Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name="Confirm password")]
+        [Compare("Password",ErrorMessage="Password and conformation password do not match.")]
+        public String ConfirmPassword { get; set; }
+    }
+}
