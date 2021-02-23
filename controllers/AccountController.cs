9f0080a56138d9d2b1476c0faadaefd1f382ad67
Brayan Baquero <brayan.baquero.cz@gmail.com>
2021-02-23 16:42:50 -0500

    Implementing login functionality  https://www.youtube.com/watch?v=9d8DXXc71RI&ab_channel=kudvenkat

--- controllers/AccountController.cs
+++ controllers/AccountController.cs
@@ -1,4 +1,5 @@
 ﻿using EmployeeManagementMVC.Models;
+using EmployeeManagementMVC.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -48,6 +49,30 @@ namespace EmployeeManagementMVC.controllers
                 {
                     ModelState.AddModelError("", error.Description);
                 }
+            }
+            return View(model);
+        }
+
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await signInManager.PasswordSignInAsync(model.Email, model.Password,model.RememberMe,false);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("index", "home");
+                }
+                
+                    ModelState.AddModelError("", "Invalid Login Attempt");
+                
             }
             return View(model);
         }
