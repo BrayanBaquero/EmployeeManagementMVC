2b69a6afdb49a7699809819d7ded03c614486047
Brayan Baquero <brayan.baquero.cz@gmail.com>
2021-03-08 16:25:08 -0500

    delete confirmation https://www.youtube.com/watch?v=hKLjt9GzYM8&ab_channel=kudvenkat

--- wwwroot/js/Custom.js
+++ wwwroot/js/Custom.js
@@ -1,1 +1,13 @@
-﻿
+﻿function confirmDelete(uniqueId, isDeleteClicked) {
+    var deleteSpan = 'deleteSpan_' + uniqueId;
+    var confirmDeleteSpan = 'confirmDeleteSpan_' + uniqueId;
+    if (isDeleteClicked) {
+        $('#' + deleteSpan).hide();
+        $('#' + confirmDeleteSpan).show();
+        //document.getElementById("deleteSpan").classList.add.toggle("hidden");
+        //document.getElementById("deleteSpan").hidden();
+    } else {
+        $('#' + deleteSpan).show();
+        $('#' + confirmDeleteSpan).hide();
+    }
+}
